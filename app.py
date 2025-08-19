7131c1d3760dd7add21729cde8e66e55dd9125ab
soaicrazy <109063273+soaicrazy@users.noreply.github.com>
2025-08-19 10:27:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 
 app = Flask(__name__)
 
-PAGE_ACCESS_TOKEN = os.getenv("EAAU0Fisjh0cBPEbpiq9JpPgZCkTmNKykol1j2jYC5AdMoxlPi0RThvTjRUHWc4ZBx3pRbSz5d8wZCtsTd8GyAZADfGfWKUmCZBJnygZAVvjvH7VgqRBURsLTZC45TWGnIaD7cQ8FfPVfjBoBZALpQMOIlc7QJnGBDTswByTba30lxvGenx72PxifPbPBkzk1X5igoWCZBl8nGZBgZDZD")
+PAGE_ACCESS_TOKEN ="EAAU0Fisjh0cBPEbpiq9JpPgZCkTmNKykol1j2jYC5AdMoxlPi0RThvTjRUHWc4ZBx3pRbSz5d8wZCtsTd8GyAZADfGfWKUmCZBJnygZAVvjvH7VgqRBURsLTZC45TWGnIaD7cQ8FfPVfjBoBZALpQMOIlc7QJnGBDTswByTba30lxvGenx72PxifPbPBkzk1X5igoWCZBl8nGZBgZDZD"
 VERIFY_TOKEN = "botchat123"
 APP_SECRET = os.getenv("APP_SECRET")
 
@@ -47,5 +47,6 @@ def send_message(psid, text):
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 3000))
     app.run(host="0.0.0.0", port=port)
+
 
 
